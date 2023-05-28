c1efdc0d21dd6f25769950acd3afe06659049c94
L2HYUNN <donghyuni98@gmail.com>
2023-05-28 13:01:16 +0900

    feat(challenge): addPageDigit 함수 구현
    addPageDigit: pageNumber를 인자로 받아 각 자릿수의 합을 출력한다.

--- Challenge/javascript-onboarding/src/problem1.js
+++ Challenge/javascript-onboarding/src/problem1.js
@@ -1,6 +1,21 @@
+/**
+ * pageNumber를 인자로 받아 각 자릿수의 합을 출력한다.
+ *
+ * @param {number} pageNumber
+ * @returns {number}
+ */
+function addPageDigit(pageNumber) {
+  return pageNumber
+    .toString()
+    .split("")
+    .reduce((acc, cur) => acc + +cur, 0);
+}
+
 function problem1(pobi, crong) {
-  var answer;
+  var answer = addPageDigit(pobi[0]);
   return answer;
 }
+
+console.log(problem1([97, 98], [197, 198]));
 
 module.exports = problem1;
