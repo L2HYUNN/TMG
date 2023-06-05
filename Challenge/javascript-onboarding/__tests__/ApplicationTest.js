f1fdc3688d077b17468b02faee87bdfb6537aac6
L2HYUNN <donghyuni98@gmail.com>
2023-06-05 11:33:25 +0900

    feat(challenge): 문제 1 테스트 케이스 추가

--- Challenge/javascript-onboarding/__tests__/ApplicationTest.js
+++ Challenge/javascript-onboarding/__tests__/ApplicationTest.js
@@ -7,16 +7,37 @@ const problem6 = require("../src/problem6");
 const problem7 = require("../src/problem7");
 
 describe("problem1", () => {
-  test("case1", () => {
+  /** 승부 검증 */
+  test("승부가 비긴 경우 0을 반환한다.", () => {
     expect(problem1([97, 98], [197, 198])).toEqual(0);
   });
-
-  test("case2", () => {
+  test("포비가 이긴 경우 1을 반환한다.", () => {
     expect(problem1([131, 132], [211, 212])).toEqual(1);
   });
+  test("크롱이 이긴 경우 2를 반환한다.", () => {
+    expect(problem1([211, 212], [131, 132])).toEqual(2);
+  });
 
-  test("case3", () => {
-    expect(problem1([99, 102], [211, 212])).toEqual(-1);
+  /** 예외 검증 */
+  test("페이지 배열의 길이는 2여야만 한다.", () => {
+    expect(problem1([121, 122, 123], [211, 212])).toEqual(-1);
+    expect(problem1([211, 212], [121, 122, 123])).toEqual(-1);
+  });
+  test("왼쪽 페이지는 홀수여야 한다.", () => {
+    expect(problem1([98, 99], [193, 194])).toEqual(-1);
+    expect(problem1([193, 194], [98, 99])).toEqual(-1);
+  });
+  test("오른쪽 페이지는 짝수여야 한다.", () => {
+    expect(problem1([99, 103], [211, 212])).toEqual(-1);
+    expect(problem1([211, 212], [99, 103])).toEqual(-1);
+  });
+  test("왼쪽 페이지는 오른쪽 페이지보다 작아야 한다.", () => {
+    expect(problem1([31, 30], [393, 394])).toEqual(-1);
+    expect(problem1([393, 394], [31, 30])).toEqual(-1);
+  });
+  test("페이지는 연속되어야 한다.", () => {
+    expect(problem1([101, 102], [393, 400])).toEqual(-1);
+    expect(problem1([393, 400], [101, 102])).toEqual(-1);
   });
 });
 
