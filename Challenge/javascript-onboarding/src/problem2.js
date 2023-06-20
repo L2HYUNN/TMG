3bb6b88312bcbba02b30d03f666a7eba894cbe6b
L2HYUNN <donghyuni98@gmail.com>
2023-06-20 22:30:58 +0900

    feat(challenge): deleteAllDuplicateCharacter 함수 구현

--- Challenge/javascript-onboarding/src/problem2.js
+++ Challenge/javascript-onboarding/src/problem2.js
@@ -1,6 +1,20 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
+function deleteAllDuplicateCharacter(string) {
+  const result = Array.from(string)
+    .filter(
+      (character, index) =>
+        string[index - 1] !== character && character !== string[index + 1]
+    )
+    .join("");
+
+  return string.length !== result.length
+    ? deleteAllDuplicateCharacter(result)
+    : result;
 }
+
+function problem2(cryptogram) {
+  return deleteAllDuplicateCharacter(cryptogram);
+}
+
+console.log(problem2("browoanoommnaon"));
 
 module.exports = problem2;
