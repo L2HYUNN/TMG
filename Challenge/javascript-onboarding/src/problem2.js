4d30137c61f58da3f4e6d40097034344dee71c2e
L2HYUNN <donghyuni98@gmail.com>
2023-06-21 11:29:27 +0900

    feat(challenge): cryptogram 유효성 체크 추가

--- Challenge/javascript-onboarding/src/problem2.js
+++ Challenge/javascript-onboarding/src/problem2.js
@@ -11,12 +11,30 @@ function deleteAllDuplicateCharacter(string) {
     : result;
 }
 
+const ErrorMessage = Object.freeze({
+  ERROR_INVALIDE_CRYPTOGRAM: "입력된 암호문의 형태가 유효하지 않습니다.",
+  ERROR_INVALIDE_CRYPTOGRAM_LENGTH: "입력된 암호문의 길이가 올바르지 않습니다.",
+});
+
+function validateCryptogram(cryptogram) {
+  if (cryptogram.toLowerCase() !== cryptogram)
+    throw new Error(ErrorMessage.ERROR_INVALIDE_CRYPTOGRAM);
+  if (cryptogram.length < 0 || cryptogram.length > 1000)
+    throw new Error(ErrorMessage.ERROR_INVALIDE_CRYPTOGRAM_LENGTH);
+}
+
 function decrypt(cryptogram) {
+  validateCryptogram(cryptogram);
+
   return deleteAllDuplicateCharacter(cryptogram);
 }
 
 function problem2(cryptogram) {
-  return decrypt(cryptogram);
+  try {
+    return decrypt(cryptogram);
+  } catch (error) {
+    return error.message;
+  }
 }
 
 console.log(problem2("browoanoommnaon"));
