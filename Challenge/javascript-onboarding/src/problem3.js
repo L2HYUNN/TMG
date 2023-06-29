9a862c537162eb463de79f984df9186bd1349f3d
L2HYUNN <donghyuni98@gmail.com>
2023-06-30 01:28:01 +0900

    feat(challenge): splitNumber 함수 구현

--- Challenge/javascript-onboarding/src/problem3.js
+++ Challenge/javascript-onboarding/src/problem3.js
@@ -1,6 +1,11 @@
-function problem3(number) {
-  var answer;
-  return answer;
+function splitNumber(number) {
+  return number.toString().split("");
 }
+
+function problem3(number) {
+  return splitNumber(number);
+}
+
+console.log(problem3(33));
 
 module.exports = problem3;
