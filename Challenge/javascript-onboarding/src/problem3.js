518bd9fdbd081d110c1fd1a01bbeec6a4cc9a8a1
L2HYUNN <donghyuni98@gmail.com>
2024-05-23 14:39:21 +0900

    feat(challenge): number 유효성 검사 로직 추가

--- Challenge/javascript-onboarding/src/problem3.js
+++ Challenge/javascript-onboarding/src/problem3.js
@@ -20,7 +20,27 @@ function indexArray(number) {
   return Array.from(Array(number), (_, index) => index);
 }
 
+const ErrorMessage = Object.freeze({
+  ERROR_INVALIDE_TYPE: "입력된 숫자의 타입이 올바르지 않습니다.",
+  ERROR_NUBER_IS_NOT_INTEGER: "입력된 숫자가 정수가 아닙니다.",
+  ERROR_NUMBER_NOT_IN_RANGE: "입력된 숫자가 유효 범위 안에 존재하지 않습니다.",
+});
+
+function validateNumber(number) {
+  if (typeof number !== "number") {
+    throw new Error(ErrorMessage.ERROR_INVALIDE_TYPE);
+  }
+  if (!Number.isInteger(number)) {
+    throw new Error(ErrorMessage.ERROR_NUBER_IS_NOT_INTEGER);
+  }
+  if (!(0 < number || number <= 10000)) {
+    throw new Error(ErrorMessage.ERROR_NUMBER_NOT_IN_RANGE);
+  }
+}
+
 function game(number) {
+  validateNumber(number);
+
   return indexArray(number).reduce(
     (result, current) => result + calculateGameNumber(current + 1),
     0
@@ -28,7 +48,11 @@ function game(number) {
 }
 
 function problem3(number) {
-  return game(number);
+  try {
+    return game(number);
+  } catch (error) {
+    return error.message;
+  }
 }
 
 console.log(problem3(33));
