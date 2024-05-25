9e5416e8cdd421d57be22a1ef017d1e97b5512cb
L2HYUNN <donghyuni98@gmail.com>
2024-05-25 15:38:02 +0900

    fix(challenge): validator import 에러 해결

--- Challenge/javascript-onboarding/src/problem3(FP)/validator.js
+++ Challenge/javascript-onboarding/src/problem3(FP)/validator.js
@@ -1,12 +1,14 @@
+const { ERROR_MESSAGE } = require("./constants");
+
 function validateNumber(number) {
   if (typeof number !== "number") {
-    throw new Error(ErrorMessage.ERROR_INVALIDE_TYPE);
+    throw new Error(ERROR_MESSAGE.ERROR_INVALIDE_TYPE);
   }
   if (!Number.isInteger(number)) {
-    throw new Error(ErrorMessage.ERROR_NUBER_IS_NOT_INTEGER);
+    throw new Error(ERROR_MESSAGE.ERROR_NUBER_IS_NOT_INTEGER);
   }
   if (!(0 < number && number <= 10000)) {
-    throw new Error(ErrorMessage.ERROR_NUMBER_NOT_IN_RANGE);
+    throw new Error(ERROR_MESSAGE.ERROR_NUMBER_NOT_IN_RANGE);
   }
 }
 
