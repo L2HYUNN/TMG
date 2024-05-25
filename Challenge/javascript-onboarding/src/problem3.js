f73dd8332de72a9f0318a4190d29e0ce388c5d2c
DongHyun Lee <donghyuni98@gmail.com>
2024-05-25 11:20:47 +0900

    [Challenge] javascript onboarding problem 03 (#2)
    * feat(challenge): 문제3 기능 구현 목록 추가
    
    * feat(challenge): splitNumber 함수 구현
    
    * feat(challenge): isGameNumber 함수 구현
    
    * feat(challenge): game 함수 구현 및 함수 추가
    
    * feat(challenge): number 유효성 검사 로직 추가
    
    * fix(challenge): number 유효성 검증 로직 수정
    
    * test(challenge): 유효성 검사 테스트 추가
    
    * docs(challenge): problem03 구현 목록 업데이트

--- Challenge/javascript-onboarding/src/problem3.js
+++ Challenge/javascript-onboarding/src/problem3.js
@@ -1,6 +1,60 @@
-function problem3(number) {
-  var answer;
-  return answer;
+const GAME = Object.freeze({ NUMBERS: [3, 6, 9] });
+
+function isGameNumber(number) {
+  return GAME.NUMBERS.includes(number);
 }
+
+function splitNumber(number) {
+  return number.toString().split("");
+}
+
+function calculateGameNumber(number) {
+  return splitNumber(number).reduce(
+    (result, splitedNumber) =>
+      isGameNumber(Number(splitedNumber)) ? result + 1 : result,
+    0
+  );
+}
+
+function indexArray(number) {
+  return Array.from(Array(number), (_, index) => index);
+}
+
+const ErrorMessage = Object.freeze({
+  ERROR_INVALIDE_TYPE: "입력된 숫자의 타입이 올바르지 않습니다.",
+  ERROR_NUBER_IS_NOT_INTEGER: "입력된 숫자가 정수가 아닙니다.",
+  ERROR_NUMBER_NOT_IN_RANGE: "입력된 숫자가 유효 범위 안에 존재하지 않습니다.",
+});
+
+function validateNumber(number) {
+  if (typeof number !== "number") {
+    throw new Error(ErrorMessage.ERROR_INVALIDE_TYPE);
+  }
+  if (!Number.isInteger(number)) {
+    throw new Error(ErrorMessage.ERROR_NUBER_IS_NOT_INTEGER);
+  }
+  if (!(0 < number && number <= 10000)) {
+    throw new Error(ErrorMessage.ERROR_NUMBER_NOT_IN_RANGE);
+  }
+}
+
+function game(number) {
+  validateNumber(number);
+
+  return indexArray(number).reduce(
+    (result, current) => result + calculateGameNumber(current + 1),
+    0
+  );
+}
+
+function problem3(number) {
+  try {
+    return game(number);
+  } catch (error) {
+    return error.message;
+  }
+}
+
+console.log(problem3(33));
 
 module.exports = problem3;
